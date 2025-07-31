206a03526ab7a0e057f8122579071c731693c669
anuragpande1977 <137955688+anuragpande1977@users.noreply.github.com>
2025-07-31 11:58:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,11 @@ st.markdown(
 # ---------------------------
 # GOOGLE SHEET CONNECTION
 # ---------------------------
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
 creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=SCOPES)
 client = gspread.authorize(creds)
 
